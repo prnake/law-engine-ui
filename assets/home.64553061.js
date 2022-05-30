0adf3d28986b08614e601e94f8979ee30d4add0d
YunYouJun <YunYouJun@users.noreply.github.com>
2022-05-30 07:01:10 +0000

    deploy: 9a5b334bc4f4d62b39e1d3ccd44470033b573414

---
+++ assets/home.64553061.js
@@ -0,0 +1,1 @@
+import{_ as n,c,g as e,o as s,G as r,E as _}from"./app.534c68be.js";const a={},m={w:"screen",h:"screen",class:"px-4 py-10 text-center"};function i(p,f){const o=r,t=_("router-view");return s(),c("main",m,[e(o),e(t)])}var d=n(a,[["render",i]]);export{d as default};
